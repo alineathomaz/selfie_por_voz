b17ec9b0883109a053bd240889739308893a4b4f
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2022-07-01 21:45:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,42 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
-  
 var recognition = new SpeechRecognition();
-
 var Textbox = document.getElementById("textbox"); 
 
 function start()
 {
-    Textbox.innerHTML = ""; 
-    recognition.start();
+  Textbox.innerHTML = ""; 
+  recognition.start();
 } 
- 
-recognition.onresult = function(event) {
 
- console.log(event); 
-
-var Content = event.results[0][0].transcript;
-
-    Textbox.innerHTML = Content;
-    console.log(Content);
-      if(Content =="selfie")
-      {
-        console.log("tirando selfie --- ");
-        speak();
-      }
+recognition.onresult = function(event)
+{
+  console.log(event); 
+  var Content = event.results[0][0].transcript;
+  Textbox.innerHTML = Content;
+  console.log(Content);
+  if(Content =="selfie")
+  {
+    console.log("tirando selfie --- ");
+    speak();
+  }
 }
 
-
-function speak(){
-    var synth = window.speechSynthesis;
-
-    speak_data = "Tirando sua selfie em 5 segundos";
-
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-
-    synth.speak(utterThis);
-
-    Webcam.attach(camera);
-
-    setTimeout(function()
-    { 
-        take_selfie(); 
-        save();
-    }, 5000);
+function speak()
+{
+  var synth = window.speechSynthesis;
+  speak_data = "Tirando sua selfie em 5 segundos";
+  var utterThis = new SpeechSynthesisUtterance(speak_data);
+  synth.speak(utterThis);
+  Webcam.attach(camera);
+  setTimeout(function()
+  { 
+    take_selfie(); 
+    save();
+  }, 5000);
 }
 
- 
 camera = document.getElementById("camera");
+
 Webcam.set({
     width:360,
     height:250,
@@ -55,11 +46,11 @@ Webcam.set({
 
 function take_selfie()
 {
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
-    });
+  Webcam.snap(function(data_uri) 
+  {
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+  });
 }
-
 
 function save()
 {
